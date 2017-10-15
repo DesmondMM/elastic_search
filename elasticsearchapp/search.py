027c21426306fcc5a8aa658a5f4f65d00d60a95c
DesmondMM <mutsakaradesmond@gmail.com>
2017-10-15 03:36:37 +0200

    added search functionality

--- elasticsearchapp/search.py
+++ elasticsearchapp/search.py
@@ -1,5 +1,5 @@
 from elasticsearch_dsl.connections import connections
-from elasticsearch_dsl import DocType, Text, Date
+from elasticsearch_dsl import DocType, Text, Date, Search
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
 
@@ -22,3 +22,9 @@ def bulk_indexing():
     BlogPostIndex.init()
     es = Elasticsearch()
     bulk(client=es, actions=(b.indexing() for b in models.BlogPost.objects.all().iterator()))
+
+
+def search(author):
+    s = Search().filter('term', author=author)
+    response = s.execute()
+    return response
